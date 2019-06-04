1caf06257525b15264f207aa909c264cc64aa235
Emily Zhang <emily@lob.com>
2019-06-04 16:54:50 -0700

    remove error message printing - cobra already prints it

--- cmd/pharos/main.go
+++ cmd/pharos/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/lob/pharos/pkg/pharos/cmd"
@@ -10,7 +9,6 @@ import (
 func main() {
 	err := cmd.Execute()
 	if err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
