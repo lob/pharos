17f2bafb8627a70547ed058c5b454a9143eb0297
Donald Little <donald@lob.com>
2019-06-17 15:57:16 -0700

    feat(clusters): add clusters CRUD endpoints (#10)

--- pkg/pharos-api-server/binder/custom_binder_test.go
+++ pkg/pharos-api-server/binder/custom_binder_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/labstack/echo"
-	"github.com/lob/pharos/internal/test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,7 +18,7 @@ func TestCustomBinderBind(t *testing.T) {
 	u := &user{}
 
 	t.Run("successfully binds payload", func(tt *testing.T) {
-		c, _ := test.NewContext(tt, echo.GET, strings.NewReader(`{"id": 123, "name": "test"}`), echo.MIMEApplicationJSON)
+		c := newContext(tt, echo.GET, strings.NewReader(`{"id": 123, "name": "test"}`), echo.MIMEApplicationJSON)
 
 		err := cb.bind(u, c)
 		assert.NoError(tt, err)
@@ -28,35 +27,35 @@ func TestCustomBinderBind(t *testing.T) {
 	})
 
 	t.Run("errors for unknown fields in payload", func(tt *testing.T) {
-		c, _ := test.NewContext(tt, echo.GET, strings.NewReader(`{"foo": "bar"}`), echo.MIMEApplicationJSON)
+		c := newContext(tt, echo.GET, strings.NewReader(`{"foo": "bar"}`), echo.MIMEApplicationJSON)
 
 		err := cb.bind(u, c)
 		assert.Contains(tt, err.Error(), `unknown field "foo"`)
 	})
 
 	t.Run("errors for unknown MIME", func(tt *testing.T) {
-		c, _ := test.NewContext(tt, echo.GET, strings.NewReader("test"), echo.MIMETextPlain)
+		c := newContext(tt, echo.GET, strings.NewReader("test"), echo.MIMETextPlain)
 
 		err := cb.bind(u, c)
 		assert.Contains(tt, err.Error(), "Unsupported Media Type")
 	})
 
 	t.Run("errors with empty payloads for non GET or DELETE requests", func(tt *testing.T) {
-		c, _ := test.NewContext(tt, echo.POST, strings.NewReader(""), echo.MIMEApplicationJSON)
+		c := newContext(tt, echo.POST, strings.NewReader(""), echo.MIMEApplicationJSON)
 
 		err := cb.bind(u, c)
 		assert.Contains(tt, err.Error(), "request body can't be empty")
 	})
 
 	t.Run("allows empty GET request", func(tt *testing.T) {
-		c, _ := test.NewContext(tt, echo.GET, strings.NewReader(""), echo.MIMEApplicationJSON)
+		c := newContext(tt, echo.GET, strings.NewReader(""), echo.MIMEApplicationJSON)
 
 		err := cb.bind(u, c)
 		assert.NoError(tt, err)
 	})
 
 	t.Run("allows empty DELETE request", func(tt *testing.T) {
-		c, _ := test.NewContext(tt, echo.DELETE, strings.NewReader(""), echo.MIMEApplicationJSON)
+		c := newContext(tt, echo.DELETE, strings.NewReader(""), echo.MIMEApplicationJSON)
 
 		err := cb.bind(u, c)
 		assert.NoError(tt, err)
